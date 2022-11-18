abb53dfd644c16bbb7ad1e97d39b2480706098e5
Jonathan Robichaud <jn692029@dal.ca>
2022-11-18 17:39:15 -0400

    Added activity switch from beach landing to check in survey
    Changed lifeguard survey from radio buttons to a spinner / drop down list.
    
    Prepped for writing to DB

--- app/src/main/java/com/example/beachbluenoser/beachLanding.java
+++ app/src/main/java/com/example/beachbluenoser/beachLanding.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -44,20 +45,24 @@ public class beachLanding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beach_landing);
-        Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
-            if(bundle.getString("beachName")!=null) {
-                beachName = bundle.getString("beachName");
 
-                Log.d("beach Main Page NAme ", " Name : " + beachName);
-            }
-        }
 
        // spinnerSetup();
+        showDataOnUI();
         getDataFromDB();
+        Button btn = (Button)findViewById(R.id.checkInSurvey);
 
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(beachLanding.this,LifeguardDataSurvey.class);
+                intent.putExtra("beachName",beachName);
 
+                startActivity(intent);
+            }
+        });
     }
+
 
     private void getDataFromDB(){
         DocumentReference landingBeachRef = db.collection("beach").document(beachName);
