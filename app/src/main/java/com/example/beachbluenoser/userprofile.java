f82c24643df537dd3240216bdbd9fdd26bd5127e
yz501694 <sy1215166128@gmail.com>
2022-12-06 01:24:19 -0400

    Display user info on User profile page

--- app/src/main/java/com/example/beachbluenoser/userprofile.java
+++ app/src/main/java/com/example/beachbluenoser/userprofile.java
@@ -10,16 +10,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
 
 public class userprofile extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -44,21 +51,21 @@ public class userprofile extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        DatabaseReference Ref = FirebaseDatabase.getInstance().getReference("users");
-        Ref.addValueEventListener(new ValueEventListener() {
+        DocumentReference Ref = db.collection("users").document("GrNKyKNkaKMagsInywkYSyA2s5m1");
+        Ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String email = String.valueOf(dataSnapshot.child("Email").getValue());
-                Email.setText(email);
-                String fullname = String.valueOf(dataSnapshot.child("Fullname").getValue());
-                FullName.setText(fullname);
-                String user = String.valueOf(dataSnapshot.child("Username").getValue());
-                username.setText(user);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        String email = documentSnapshot.getData().get("Email").toString();
+                        Email.setText(email);
+                        String name = documentSnapshot.getData().get("Fullname").toString();
+                        FullName.setText(name);
+                        String user = documentSnapshot.getData().get("Username").toString();
+                        username.setText(user);
+                    }
+                }
             }
         });
 
